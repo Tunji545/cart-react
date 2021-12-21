ddff73b4406fd32f0f11c12e5442d2b21db60c82
Olatunji Omisakin <msknolatunji@gmail.com>
2021-12-21 12:14:38 +0100

    complete react Stripe Cart project

---
+++ src/reducer.js
@@ -0,0 +1,70 @@
+const reducer = (state, action) => {
+  if (action.type === 'CLEAR') {
+    return { ...state, cart: [] };
+  }
+  if (action.type === 'REMOVE_ITEM') {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
+  // if (action.type === 'INCREASE') {
+  //   const newItem = state.cart.map((item) => {
+  //     if (item.id === action.payload) {
+  //       return { ...item, amount: item.amount + 1 };
+  //     }
+  //     return item;
+  //   });
+  //   return { ...state, cart: newItem };
+  // }
+  // if (action.type === 'DECREASE') {
+  //   const newItem = state.cart
+  //     .map((item) => {
+  //       if (item.id === action.payload) {
+  //         return { ...item, amount: item.amount - 1 };
+  //       }
+  //       return item;
+  //     })
+  //     .filter((item) => item.amount !== 0);
+  //   return { ...state, cart: newItem };
+  // }
+  if (action.type === 'TOTALS') {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const totalPrice = price * amount;
+        cartTotal.total += totalPrice;
+        cartTotal.amount += amount;
+        return cartTotal;
+      },
+      { total: 0, amount: 0 }
+    );
+    total = +total.toFixed(2);
+    return { ...state, total, amount };
+  }
+  if (action.type === 'LOADING') {
+    return { ...state, loading: true };
+  }
+  if (action.type === 'FETCH') {
+    return { ...state, cart: action.payload, loading: false };
+  }
+  if (action.type === 'TOGGLE_AMOUNT') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === 'inc') {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === 'dec') {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  throw new Error('No incorrect action type');
+};
+
+export default reducer;
